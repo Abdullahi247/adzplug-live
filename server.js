a4c151971219f224f6e4063adbce05054cd4d423
Abdullahi <tsmart247@gmail.com>
2022-07-25 14:40:49 +0100

    LIVE STREAM

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express()
 const Database = require('./Database/Database')
 Database()
 
+app.use('/*', (req, res) => {
+     res.sendFile(path.join(__dirname + '/ThankYou.html'))
+})
+
+app.listen(8080, () => {
+    console.log("Live Stream Connected Successfully")
+})
+
 const node_media_server = require('./Routers/Live Stream/live_Server')
 
 node_media_server.run()
